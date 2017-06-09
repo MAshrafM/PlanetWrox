5dc995b53b1029e87897e1bcc926a603fc818565
MAshraf <ee.mashraf@gmail.com>
2017-06-09 08:59:23 +0300

    LINQ and ADO.NET

--- Reviews/All.aspx
+++ Reviews/All.aspx
@@ -4,4 +4,9 @@
 </asp:Content>
 
 <asp:Content ID="Content2" ContentPlaceHolderID="cpMainContent" Runat="Server">
+    <asp:GridView ID="GridView1" runat="server">
+</asp:GridView>
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server"></asp:SqlDataSource>
+    <asp:EntityDataSource ID="EntityDataSource1" runat="server">
+    </asp:EntityDataSource>
 </asp:Content>
--- Reviews/All.aspx.vb
+++ Reviews/All.aspx.vb
@@ -1,5 +1,15 @@
-﻿
+﻿Imports PlanetWroxModel
 Partial Class Reviews_All
     Inherits System.Web.UI.Page
 
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Using myEntities As New PlanetWroxEntities()
+            Dim authorizedReviews = From review In myEntities.Reviews
+                                    Where review.Authorized= True
+                                    Order By review.CreateDateTime Descending
+                                    Select review
+            GridView1.DataSource = authorizedReviews
+            GridView1.DataBind()
+        End Using
+    End Sub
 End Class
