610fa2e4496aa79c707ace77bbcda2b4e1603256
MAshraf <ee.mashraf@gmail.com>
2017-06-09 10:15:38 +0300

    customize list view

--- ManagePhotoAlbum.aspx.vb
+++ ManagePhotoAlbum.aspx.vb
@@ -7,5 +7,22 @@ Partial Class ManagePhotoAlbum
         Dim photoAlbumId As Integer = Convert.ToInt32(Request.QueryString.Get("PhotoAlbumId"))
         Dim myPicture As Picture = CType(e.Entity, Picture)
         myPicture.PhotoAlbumId = photoAlbumId
+
+        Dim FileUpload1 As FileUpload = CType(ListView1.InsertItem.FindControl("FileUpload1"), FileUpload)
+        Dim virtualFolder As String = "~/GigPics"
+        Dim physicalFolder As String = Server.MapPath(virtualFolder)
+        Dim FileName As String = Guid.NewGuid().ToString()
+        Dim extension As String = System.IO.Path.GetExtension(FileUpload1.FileName)
+        FileUpload1.SaveAs(System.IO.Path.Combine(physicalFolder, FileName + extension))
+        myPicture.ImageUrl = virtualFolder + FileName + extension
+    End Sub
+
+    Protected Sub ListView1_ItemInserting(sender As Object, e As ListViewInsertEventArgs) Handles ListView1.ItemInserting
+        Dim FileUpload1 As FileUpload = CType(ListView1.InsertItem.FindControl("FileUpload1"), FileUpload)
+        If Not FileUpload1.HasFile OrElse Not FileUpload1.FileName.ToLower().EndsWith(".jpg") Then
+            Dim cusValImage As CustomValidator = CType(ListView1.InsertItem.FindControl("cusValImage"), CustomValidator)
+            cusValImage.IsValid = False
+            e.Cancel = True
+        End If
     End Sub
 End Class
