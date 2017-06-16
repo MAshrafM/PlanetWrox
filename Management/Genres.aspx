cb7180b6a0bdb1ccb7318559fd5be727f73f80e7
MAshraf <ee.mashraf@gmail.com>
2017-06-16 08:29:29 +0300

    error handling

--- Management/Genres.aspx
+++ Management/Genres.aspx
@@ -3,7 +3,7 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="cpMainContent" Runat="Server">
-
+    <asp:Label ID="ErrorMessage" runat="server" CssClass="ErrorMessage" EnableViewState="false"></asp:Label>
     <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="Id" DataSourceID="SqlDataSource1" EmptyDataText="There are no data records to display." AllowPaging="True" AllowSorting="True" CellPadding="4" ForeColor="#333333" GridLines="None">
         <AlternatingRowStyle BackColor="White" />
     <Columns>
--- Management/Genres.aspx.vb
+++ Management/Genres.aspx.vb
@@ -1,4 +1,6 @@
 ﻿Imports System.Data
+Imports System.Data.SqlClient
+
 Partial Class Management_Genres
     Inherits System.Web.UI.Page
 
@@ -10,9 +12,20 @@ Partial Class Management_Genres
                     Dim deleteLink As LinkButton =
                     TryCast(e.Row.FindControl("DeleteLink"), LinkButton)
                     If deleteLink IsNot Nothing Then
-                        deleteLink.Enabled = False
+                        '  deleteLink.Enabled = False
                     End If
                 End If
         End Select
+    End Sub
+
+    Protected Sub SqlDataSource1_Deleted(sender As Object, e As SqlDataSourceStatusEventArgs) Handles SqlDataSource1.Deleted
+        If e.Exception IsNot Nothing AndAlso TypeOf (e.Exception) Is SqlException Then
+            Dim myException As SqlException = CType(e.Exception, SqlException)
+            If myException.Number = 547 Then
+                ErrorMessage.Text = "Sorry, you can't delete this genre because " &
+                "it has associated reviews that you need to delete first."
+                e.ExceptionHandled = True
+            End If
+        End If
     End Sub
 End Class
