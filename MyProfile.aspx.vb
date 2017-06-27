e7a331b0a6b089cc24f2dc5b8e52ac0a80cb3e68
MAshraf <ee.mashraf@gmail.com>
2017-06-27 08:29:53 +0300

    creating a profile

--- MyProfile.aspx.vb
+++ MyProfile.aspx.vb
@@ -2,4 +2,8 @@
 Partial Class MyProfile
     Inherits System.Web.UI.Page
 
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Profile.FirstName = "Your first name here"
+        Profile.LastName = "Your last name here"
+    End Sub
 End Class
