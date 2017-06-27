e3781d296dfd46475ab2098a3b4e7aa76c9a4908
MAshraf <ee.mashraf@gmail.com>
2017-06-27 09:01:57 +0300

    reviews from profile

--- Reviews/Default.aspx.vb
+++ Reviews/Default.aspx.vb
@@ -1,5 +1,21 @@
-﻿
+﻿Imports PlanetWroxModel
+
+
 Partial Class Reviews_Default
     Inherits System.Web.UI.Page
 
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Using myEntities As PlanetWroxEntities = New PlanetWroxEntities()
+            If Profile.FavoriteGenres.Count > 0 Then
+                Dim favGenres = From genre In myEntities.Genres.Include("Reviews")
+                Order By genre.Name
+                Where Profile.FavoriteGenres.Contains(genre.Id)
+                Select New With {genre.Name, genre.Reviews}
+                GenreRepeater.DataSource = favGenres
+                GenreRepeater.DataBind()
+            End If
+            GenreRepeater.Visible = GenreRepeater.Items.Count > 0
+            NoRecords.Visible = Not GenreRepeater.Visible
+        End Using
+    End Sub
 End Class
